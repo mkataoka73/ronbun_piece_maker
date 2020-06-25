cdefd059710bc7379aeed863bf5bff54d5441af6
Masayoshi Kataoka <mkataoka73@gmail.com>
2020-06-25 13:18:33 +0900

    fix validation

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,4 +1,14 @@
+# == Schema Information
+#
+# Table name: elements
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  citation   :text
+#  source     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Element < ApplicationRecord
-  validates_presence_of :title, :citation, :source,
-                        :on => :create
+  validates_presence_of :title, :citation, :source
 end
